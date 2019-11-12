63dd8bfce62afd6756000920d77313ea13654ec4
ello-anand <anand.thrithvam@outlook.com>
2019-11-12 10:15:07 +0530

    Add project files.

---
+++ PaTS/PaTS/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using PaTS.Models;
+
+namespace PaTS.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
