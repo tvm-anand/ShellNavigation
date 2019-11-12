14590649ef5a265973da4bcf9a723ce9230d47cb
ello-anand <anand.thrithvam@outlook.com>
2019-11-12 13:51:13 +0530

    Changes

--- PaTS/PaTS/Views/ItemsPage.xaml.cs
+++ PaTS/PaTS/Views/ItemsPage.xaml.cs
@@ -18,25 +18,22 @@ namespace PaTS.Views
     [DesignTimeVisible(false)]
     public partial class ItemsPage : ContentPage
     {
-        ItemsViewModel viewModel;
+        
 
         public ItemsPage()
         {
             InitializeComponent();
 
-            BindingContext = viewModel = new ItemsViewModel();
+            
         }
 
-        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+       public async void Timesheet_Tapped(object sender, EventArgs args)
         {
-            var item = args.SelectedItem as Item;
-            if (item == null)
-                return;
+       
 
-            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+            await Navigation.PushAsync(new ItemDetailPage());
 
-            // Manually deselect item.
-            ItemsListView.SelectedItem = null;
+          
         }
 
         async void AddItem_Clicked(object sender, EventArgs e)
@@ -44,12 +41,6 @@ namespace PaTS.Views
             await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
         }
 
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            if (viewModel.Items.Count == 0)
-                viewModel.LoadItemsCommand.Execute(null);
-        }
+    
     }
 }
